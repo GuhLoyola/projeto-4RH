1638cdcb7e116d95950e97b23d34227f68addd9c
Gustavo Loyola <guga.loyola17@gmail.com>
2024-05-02 23:41:56 -0300

    Colunas da tabela adicionadas ao projeto

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { getData } from "../services/getData";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import Cell from "./Cell";
 
 
 interface Product {
@@ -12,7 +14,7 @@ interface Product {
 
 const Table = () => {
 
-    const [product, setProducts] = useState<Product[]>([])
+    const [products, setProducts] = useState<Product[]>([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,8 +33,70 @@ const Table = () => {
 
         fetchData()
     }, [])
+
+    const columns = [
+        {
+            accessorKey: 'portfolioProductId',
+            header: "Id",
+            cell: Cell
+        },
+        {
+            accessorKey: 'productName',
+            header: "Nome do Produto",
+            size: 450,
+            cell: Cell
+        },
+        {
+            accessorKey: 'correctedQuota',
+            header: "Cota Corrigida",
+            cell: Cell
+        },
+        {
+            accessorKey: 'value',
+            header: "Valor",
+            cell: Cell
+        }
+    ]
+
+    const tabela = useReactTable({
+        data: products,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        columnResizeMode: "onChange"
+    })
+
     return (
-        <Box>Dale</Box>
+        <Box className="container">
+            <Box className="table" w={tabela.getTotalSize()}>
+                {tabela.getHeaderGroups().map((headerGroup) =>
+                    <tr key={headerGroup.id} className="tr">
+                        {headerGroup.headers.map((header) =>
+                            <Box key={header.id} className="th" w={header.getSize()}>
+                                {header.column.columnDef.header}
+                                <div
+                                    onTouchStart={header.getResizeHandler()}
+                                    onMouseDown={header.getResizeHandler()}
+                                    className={
+                                        `resizer ${header.column.getIsResizing() ? "isResizing" : ""
+                                        }`
+                                    } />
+                            </Box>
+                        )}
+                    </tr>
+                )}
+                {
+                    tabela.getRowModel().rows.map((row) => (
+                        <tr className="tr" key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <Box className="td" key={cell.id} w={cell.column.getSize()}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </Box>
+                            ))}
+                        </tr>
+                    ))
+                }
+            </Box>
+        </Box>
     )
 }
 
