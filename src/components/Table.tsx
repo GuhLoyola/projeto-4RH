b4922417a6465673d5db9353659858dfeede0ddd
Gustavo Loyola <guga.loyola17@gmail.com>
2024-05-03 16:35:20 -0300

    Table alterado para se ajustar mais ao TypeScript

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import { Box, Icon } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { getData } from "../services/getData";
-import { flexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import { createColumnHelper, flexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
 import Cell from "./Cell";
 import Filters from "./Filters";
 import SortIcon from "./icons/SortIcon";
@@ -16,8 +16,10 @@ interface Product {
 
 const Table = () => {
 
-    const [products, setProducts] = useState<Product[]>([])
+    const [products, setProducts] = useState([])
     const [filtering, setFiltering] = useState('')
+
+    const columnHelper = createColumnHelper<Product>()
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,28 +38,27 @@ const Table = () => {
     }, [])
 
     const columns = [
-        {
-            accessorKey: 'portfolioProductId',
-            header: "Id",
+
+        columnHelper.accessor('portfolioProductId', {
+            header: () => "Id",
             cell: Cell
-        },
-        {
-            accessorKey: 'productName',
-            header: "Nome do Produto",
+        }),
+        columnHelper.accessor('productName', {
+            header: () => "Nome do Produto",
             size: 450,
             cell: Cell
-        },
-        {
-            accessorKey: 'correctedQuota',
-            header: "Cota Corrigida",
+        }),
+        columnHelper.accessor('correctedQuota', {
+            header: () => "Cota Corrigida",
             size: 170,
             cell: Cell
-        },
-        {
-            accessorKey: 'value',
-            header: "Valor",
+        }),
+        columnHelper.accessor('value', {
+            header: () => "Valor",
             cell: Cell
-        }
+        }),
+
+
     ]
 
     const tabela = useReactTable({
@@ -81,25 +82,30 @@ const Table = () => {
             />
 
             <Box className="table" w={tabela.getTotalSize()}>
-                {tabela.getHeaderGroups().map((headerGroup) =>
-                    <tr key={headerGroup.id} className="tr">
-                        {headerGroup.headers.map((header) =>
-                            <Box key={header.id} className="th" w={header.getSize()}>
-                                {header.column.columnDef.header}
-                                {
-                                    header.column.getCanSort() &&
-                                    <Icon as={SortIcon} mx={3} fontSize={14} onClick={header.column.getToggleSortingHandler()} />
-                                }
-                                <div
-                                    onMouseDown={header.getResizeHandler()}
-                                    onTouchStart={header.getResizeHandler()}
-                                    className={`resizer ${header.column.getIsResizing() ? "isResizing" : ""
-                                        }`}
-                                />
-                            </Box>
-                        )}
-                    </tr>
-                )}
+
+                <thead>
+                    {tabela.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id} className="tr">
+                            {headerGroup.headers.map((header) => (
+                                <Box key={header.id} className="th" w={header.getSize()}>
+                                    <div>
+                                        {flexRender(header.column.columnDef.header, header.getContext())}
+                                        {
+                                            header.column.getCanSort() &&
+                                            <Icon as={SortIcon} mx={3} fontSize={14} onClick={header.column.getToggleSortingHandler()} />
+                                        }
+                                        <div
+                                            onMouseDown={header.getResizeHandler()}
+                                            onTouchStart={header.getResizeHandler()}
+                                            className={`resizer ${header.column.getIsResizing() ? "isResizing" : ""
+                                                }`}
+                                        />
+                                    </div>
+                                </Box>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
                 {
                     tabela.getRowModel().rows.map((row) => (
                         <tr className="tr" key={row.id}>
