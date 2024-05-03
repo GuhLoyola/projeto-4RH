330ef896a8e73906a7ebe1281858755a96ea2d11
Gustavo Loyola <guga.loyola17@gmail.com>
2024-05-02 21:41:31 -0300

    Componente Table criado

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,39 @@
+import { Box } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { getData } from "../services/getData";
+
+
+interface Product {
+    portfolioProductId: string;
+    productName: string;
+    correctedQuota: number;
+    value: number;
+}
+
 const Table = () => {
-  return (
-    <div>Table</div>
-  )
+
+    const [product, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await getData()
+                const productData = await response.data.dailyEquityByPortfolioChartData
+
+                setProducts(productData)
+
+                console.log(productData)
+
+            } catch (error) {
+                throw new Error('Houve um problema ao listar os dados')
+            }
+        }
+
+        fetchData()
+    }, [])
+    return (
+        <Box>Dale</Box>
+    )
 }
 
 export default Table
